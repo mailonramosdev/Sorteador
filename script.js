22ee55be606894e82a43670d0ec8d6f1f3de5d93
mailonramosdev <mailonramosdev>
2026-05-14 14:48:03 -0300

    Fiz alterações

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function generateNumber(){
     const min = Math.ceil(document.querySelector(".input-min").value)
       const max = Math.floor(document.querySelector(".input-max").value)
 
+      if( min,  max ) {
+        alert ("O valor minino tem que ser MENOR que o valor maximo")
+      } else {}
+        
+      console.log(min, max)
+
       const result = Math.floor(Math.random() * (max - min + 1)) + min;
         alert(result)
 }
